ef638ad42406965aa51476484d99a1b1de5bb266
jasonasante <jason.asante@outlook.com>
2023-10-04 12:58:13 +0100

    remove server folder

--- task2/src/App.jsx
+++ task2/src/App.jsx
@@ -1,21 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/navbar/Navbar';
 import MainSection from './components/mainSection/MainSection';
 import Thanks from './components/thanks/Thanks';
 function App() {
-  const [applicant, setApplicant]=useState("")
-  useEffect(() => {
-    fetch("http://localhost:8000/")
-      .then((res) => res.json())
-      .then((data) => console.log('running') );
-  }, []);
+  const [applicant, setApplicant] = useState("")
+
   return (
     <div className="App">
-      <Navbar/>
-      <MainSection setApplicant={setApplicant}/>
+      <Navbar />
+      <MainSection setApplicant={setApplicant} />
       {applicant &&
-      
-      <Thanks applicant={applicant}/>
+
+        <Thanks applicant={applicant} />
       }
     </div>
   );
